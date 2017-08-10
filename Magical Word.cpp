4a74a9a76ed5abf6e09ea18522651fb277c751ce
Charu Agarwal <charu.agarwal@squareboat.com>
2017-08-10 09:18:15 +0530

    More.

--- Magical Word.cpp
+++ Magical Word.cpp
@@ -2,48 +2,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int cal_zeroes(int n){
-  int base = 5;
-  int sum = 0;
-  while( n/base )
-  {
-    sum += n/base;
-    base *= 5;
-  }
-  return sum;
-}
 
-int arr[500000];
+int nearestPrime(int n){
+  int a[]={67,71,73,79,83,89,97,101, 103, 107, 109, 113};
+  int diff=255;
+  int p;
+  for(int i=0; i<12; i++){
+    if(abs(n-a[i])<diff)
+    {
+      diff = abs(n-a[i]);
+      p=a[i];
+
+    }
+
+  }
+  return p;
+}
 
 int main()
 {
-  map<int, pair<int, int> > myMap;
-  int lastVal=1;
-  int first=5;
-  int last;
-  for(int i=6; i<500000; i++)
-  {
-    int temp = cal_zeroes(i);
-    if(temp!= lastVal)
-    {
-      myMap.insert(make_pair(lastVal, make_pair(first, i-1)));
-      lastVal= temp;
-      first =i;
-    }
-    last =i;
-
-  }
-  myMap.insert(make_pair(lastVal, make_pair(first, last-1)));
-
   int t;
   cin>>t;
   while(t--){
     int n;
     cin>>n;
-    cout<<myMap[n].second - myMap[n].first + 1<<endl;
-    for(int i= myMap[n].first; i<=myMap[n].second; i++)
-    cout<<i<<" ";
-    cout<<endl;
+    string s;
+    cin>>s;
+    for(int i=0; i<n; i++){
+      s[i]= nearestPrime(s[i]);
+    }
+    cout<<s<<endl;
   }
-  return 0;
+
 }
